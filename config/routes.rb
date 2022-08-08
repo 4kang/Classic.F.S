90b513f78505c939f9bd6cf10c0022ae139fc4fa
4kang <106650699+4kang@users.noreply.github.com>
2022-08-08 17:40:00 +0000

    Add Top

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  # 顧客用
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    root :to => 'homes#top'
+  end
 end
