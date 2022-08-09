aca2152db8f1de4f075b3a1b53b34b817558ab10
4kang <106650699+4kang@users.noreply.github.com>
2022-08-09 07:15:29 +0000

    dbMigrate

--- db/migrate/20220809061025_create_addresses.rb
+++ db/migrate/20220809061025_create_addresses.rb
@@ -1,6 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
+      
+      t.integer :customer_id, null: false
+      t.string :address, null: false
+      t.string :post_code, null: false
+      t.string :name, null: false
+      t.string :telephone_number, null: false
 
       t.timestamps
     end
--- db/migrate/20220809061323_create_orders.rb
+++ db/migrate/20220809061323_create_orders.rb
@@ -2,6 +2,16 @@ class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
 
+      t.integer :customer_id, null: false
+      t.string :name, null: false
+      t.string :post_code, null: false
+      t.string :address, null: false
+      t.string :telephone_number, null: false
+      t.integer :shipping_fee, null: false
+      t.integer :pay_total, null: false
+      t.integer :payment_method, null: false, default: 0
+      t.integer :status, null: false, default: 0
+
       t.timestamps
     end
   end
