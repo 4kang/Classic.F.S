20b7361e32ad44e83dd36734fdccc1843a73f921
4kang <106650699+4kang@users.noreply.github.com>
2022-08-10 03:19:53 +0000

    customer migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  
+
   # 管理者用
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   # 顧客用
   devise_for :customers, controllers: {
     registrations: "public/registrations",
@@ -13,5 +13,32 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: :public do
     root :to => 'homes#top'
+    # customer
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/my_page/edit' => 'customers#edit'
+    patch 'customers/my_page/complete' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    # item
+    resources :items, only:[:index, :show]
+    # cart_items
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :cart_items, only:[:index, :update, :destroy, :create]
+    # orders
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete'
+    resources :orders, only:[:new, :create, :index, :show]
+    # address
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
   end
+
+  namespace :admin do
+    get "" => "homes#top"
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :items, only:[:index, :new, :create,]
+    resources :orders, only:[:show, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :order_details, only:[:update]
+  end
+
 end
