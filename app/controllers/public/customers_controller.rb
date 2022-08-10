a44a69cb57d8abee4f643de7f058ed31f3b09b60
4kang <106650699+4kang@users.noreply.github.com>
2022-08-10 10:35:02 +0000

    Add Customer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,19 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @customer = current_customer
+  end
+
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    
+  end
+  
+  def unsubscribe
+    @customer = current_customer
+  end
+
 end
