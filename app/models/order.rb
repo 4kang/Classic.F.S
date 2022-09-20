ac4bbc783824fd631c05cfdea267b6eed3885d3a
4kang <106650699+4kang@users.noreply.github.com>
2022-09-20 08:10:39 +0000

    Add

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,10 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
 
+  # 支払方法｛クレジットカード、銀行振込｝
   enum payment_method: { credit_card: 0, transfer: 1 }
+
+  # 注文ステータス｛入金待ち、入金確認、準備中、発送準備中｝
   enum status: {waiting_for_deposit: 0, payment_confirmation: 1, in_production: 2, preparing_to_ship: 3, sent: 4}
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,6 +3,7 @@ class OrderDetail < ApplicationRecord
   belongs_to :order
   belongs_to :item
 
+  # 製作ステータス｛着手不可、準備待ち、準備中、完了｝
   enum making_status: { not_startable: 0, wait_product: 1, in_product: 2, finish_product: 3}
 
   def subtotal
