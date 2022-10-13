05ac880e462a51c92ff28c9318f56d4d98ca0e9f
4kang <106650699+4kang@users.noreply.github.com>
2022-10-13 08:27:02 +0000

    Add logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # 許可されたパラメータを構成する
+  # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで、ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :telephone_number, :is_deleted])
   end
